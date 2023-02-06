ba6d316d5382be77fad5d4d69e6a4b770def510e
Crystal Sun <crysun@umich.edu>
2023-02-06 17:44:37 -0500

    Swift UI Starter Files

---
+++ TextTalk/TextTalk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextTalk
+//
+//  Created by Crystal Sun on 2/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextTalk/TextTalk/TextTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextTalkApp.swift
+//  TextTalk
+//
+//  Created by Crystal Sun on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TextTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
