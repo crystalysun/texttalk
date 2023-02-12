c7dfbe2c9676ee500b646cfb6b22e85deed9bef6
Crystal Sun <crysun@umich.edu>
2023-02-11 19:29:30 -0500

    Fix build errors

--- TextTalk/TextTalk/TextToSpeech.swift
+++ TextTalk/TextTalk/TextToSpeech.swift
@@ -22,10 +22,10 @@ func TTS(text: String, accent: String, speed: Float, gen: String){
     }
 
     utterance.rate = speed
-    utterance.voice = AVSpeechSynthesisVoice(gender: gen)
-    let synthesizer = AVSpeechSyntheziser()
+    // utterance.voice = AVSpeechSynthesisVoice(gender: gen)
+    // let synthesizer = AVSpeechSyntheziser()
     
-    synthesizer.speech(utterance)
+    // synthesizer.speech(utterance)
 }
 
 TTS(text: "Hi good morning", accent: "American", speed: 1.0, gen: "female")
