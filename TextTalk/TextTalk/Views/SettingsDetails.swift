e8c907b06a165f2ca95aa4bfdb8deb5924cfa945
Crystal Sun <crysun@umich.edu>
2023-03-17 10:51:52 -0400

    Adding phrases to the saved phrases array

--- TextTalk/TextTalk/Views/SettingsDetails.swift
+++ TextTalk/TextTalk/Views/SettingsDetails.swift
@@ -7,20 +7,49 @@
 
 import SwiftUI
 
-var phrases: [Phrase] = [Phrase(id: 1, content: "Hi"), Phrase(id: 2, content: "Hello"), Phrase(id: 3, content: "Good morning"), Phrase(id: 4, content: "Good night"), Phrase(id: 5, content: "Bye"), Phrase(id: 6, content: "Thank you")]
+var phraseGlobal = Phrases()
+var idNum = 7
 
 struct SettingsDetails: View {
     var pageName: String
+    @State private var addPhrase: Bool = false
+    @ObservedObject var phrases = phraseGlobal
 
+    private struct AddPhraseSheet: View {
+        @State var input: String = ""
+        @Environment(\.dismiss) private var dismiss
+        @ObservedObject var phrases = phraseGlobal
+
+        var body: some View {
+            VStack {
+                Text("Add a new custom phrase")
+                TextField("Enter phrase here", text: $input)
+                Button("Add") {
+                    phrases.append(id: idNum, content: input)
+                    idNum += 1
+                    dismiss()
+                    input = ""
+                }
+            }
+        }
+    }
+    
     var body: some View {
         if (pageName == "Saved Phrases") {
-            List(phrases) { phrase in
+            List(phrases.data.sorted(by: { $0 < $1 })) { phrase in
                 Text(phrase.content)
             }
             .navigationTitle("Saved Phrases")
         }
         else {
             Text(pageName)
+        }
+        
+        Button("Add New Phrase") {
+            addPhrase = true
+        }
+        .sheet(isPresented: $addPhrase) {
+            AddPhraseSheet()
         }
     }
 }
