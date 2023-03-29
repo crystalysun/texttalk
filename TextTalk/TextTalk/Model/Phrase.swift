7791f93e3cf8743f80f01ce1399bba056f9bd3d3
Crystal Sun <crysun@umich.edu>
2023-03-28 22:52:03 -0400

    update saved phrases db

--- TextTalk/TextTalk/Model/Phrase.swift
+++ TextTalk/TextTalk/Model/Phrase.swift
@@ -28,12 +28,24 @@ class Phrase: Identifiable, Comparable {
 }
 
 class Phrases: ObservableObject  {
-    @Published var data: [Phrase] = [ Phrase(id: 1, content: "Hi"),
-                                      Phrase(id: 2, content: "Hello"),
-                                      Phrase(id: 3, content: "Good morning"),
-                                      Phrase(id: 4, content: "Good night"),
-                                      Phrase(id: 5, content: "Bye"),
-                                      Phrase(id: 6, content: "Thank you") ]
+    @Published var data: [Phrase] = [ Phrase(id: 1, content: "Hello!"),
+                                      Phrase(id: 2, content: "How are you?"),
+                                      Phrase(id: 3, content: "What's up?"),
+                                      Phrase(id: 4, content: "Good morning!"),
+                                      Phrase(id: 5, content: "Good afternoon!"),
+                                      Phrase(id: 6, content: "Good night!"),
+                                      Phrase(id: 7, content: "May I ask who is calling, please?"),
+                                      Phrase(id: 8, content: "How can I help?"),
+                                      Phrase(id: 9, content: "What can I do for you?"),
+                                      Phrase(id: 10, content: "One moment please."),
+                                      Phrase(id: 11, content: "Could you please repeat that?"),
+                                      Phrase(id: 12, content: "Would you like to leave a message?"),
+                                      Phrase(id: 13, content: "Could I leave a message?"),
+                                      Phrase(id: 14, content: "Nice to hear from you!"),
+                                      Phrase(id: 15, content: "I have to let you go now."),
+                                      Phrase(id: 16, content: "Talk again soon!"),
+                                      Phrase(id: 17, content: "Bye!"),
+                                      Phrase(id: 18, content: "Thank you!") ]
     
     public func append(id: Int, content: String) {
         self.data.append(Phrase(id: id, content: content))
