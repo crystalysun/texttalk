b4d83e5f1fdb239e236ce5ed683ae75fb9c6e053
Crystal Sun <crysun@umich.edu>
2023-04-16 00:39:02 -0400

    Add in reset password and help functionality

--- TextTalk/TextTalk/Views/SettingsDetails.swift
+++ TextTalk/TextTalk/Views/SettingsDetails.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct SettingsDetails: View {
     var pageName: String
+    @Binding var passwordSet : Bool
+    @Binding var key : String
+    @State private var createPass: Bool = false
+    @State private var wrongPass = false
+    @State private var helpSheet = false
     
     var body: some View {
         if (pageName == "Saved Phrases") {
@@ -17,13 +22,37 @@ struct SettingsDetails: View {
         else if(pageName == "Customize Voice") {
             VoiceSettingsView()
         }
-        
+        else if (pageName == "Password Settings") {
+            VStack {
+                Text("Password Settings")
+                    .font(.title)
+                    .padding()
+                
+                Button("Reset Password") {
+                    createPass = true
+                }
+                    .sheet(isPresented: $createPass) {
+                        CreatePassSheet(passwordSet: $passwordSet, key: $key, createPass: $createPass, wrongPass: $wrongPass)
+                    }
+                    .padding()
+                    .buttonStyle(.borderedProminent)
+                
+                Button("Help") {
+                    helpSheet = true
+                }
+                    .sheet(isPresented: $helpSheet) {
+                        HelpSheet()
+                    }
+                    .padding()
+                    .buttonStyle(.borderedProminent)
+            }
+        }
         
     }
 }
 
-struct SettingsDetails_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsDetails(pageName: "Saved Phrases")
-    }
-}
+//struct SettingsDetails_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SettingsDetails(pageName: "Saved Phrases")
+//    }
+//}
